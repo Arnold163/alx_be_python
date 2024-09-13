4f9533e046c7b047ac0ae8a84db6a0d74d4bd960
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-09-13 03:46:30 +0200

    basic ops

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#define the variables 
+number1 = 10
+number2 = 5
+
+#sum calculation
+sum_result = number1 + number2
+print(f"sum of {number1} and {number2} is {sum_result}")
+
+#difference calculation
+difference_result = number1 - number2
+print(f"Difference of {number1} and {number2} is {difference_result}")
+
+#product calculation 
+product_result = number1 * number2
+print(f"Product of {number1} and {number2} is {product_result}")
