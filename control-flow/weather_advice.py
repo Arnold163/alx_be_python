2abc70abb2c6797febbf710116de9189646badbe
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-09-21 09:19:37 +0200

    correct spelling

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,5 +1,5 @@
 # ask user for weather
-weather = input("What is the weather like today? (sunny/cloudy/rainy): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses")
