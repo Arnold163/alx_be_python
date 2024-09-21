0cb20e6f35bee11e52cd610701284d7a7f24926d
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-09-21 11:10:47 +0200

    forggotten reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,13 +5,13 @@ time_bound = input("is it time-bound? (yes/no): ").lower()
 
 match priority:
     case "high":
-        reminder = f"The task '{task}' is high priority."
+        reminder = f"Reminder: The task '{task}' is high priority."
     case "medium":
-        reminder = f"The task '{task}' is medium priority."
+        reminder = f"Reminder: The task '{task}' is medium priority."
     case "low":
-        reminder = f"The task '{task}' is low priority."
+        reminder = f"Reminder: The task '{task}' is low priority."
     case _:
-        reminder = "Invalid priority level entered."
+        reminder = "Reminder: Invalid priority level entered."
 
 if time_bound == "yes" and priority in ["high", "medium", "low"]:
     reminder = reminder + " It requires immidiate attention today."
