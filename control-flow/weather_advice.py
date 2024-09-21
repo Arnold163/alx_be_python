566b865863a31a95dde7e2bd08650a12007f21f9
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-09-21 09:31:27 +0200

    new changes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -2,7 +2,7 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if weather == "sunny":
-    print("Wear a t-shirt and sunglasses")
+    print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
     print("Dont forgget your umbrella and raincoat.")
 elif weather == "cold":
