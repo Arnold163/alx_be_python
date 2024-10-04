879a390d5271718bde6271ff7de593869f67916b
Ärnold Phiri <aphiri@pressethanolmw.com>
2024-10-04 10:41:17 +0200

    multplication unitest

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,38 @@
+#simple calculator 
+import unittest
+from test_simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 0), 0)  
+        self.assertEqual(self.calc.add(0, 0), 0)     
+        self.assertEqual(self.calc.add(-5, -7), -12)   
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-5, -3), -2)
+
+    def test_multiplication(self):
+        """Test the multiplication method."""
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-3, -7), 21)
+
+    def test_division(self):
+        """Test the division method."""
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+        # Test for division by zero
+        self.assertIsNone(self.calc.divide(5, 0))
+
+if __name__ == '__main__':
+    unittest.main()
